034e6256066278baea5ce14149cf958f83485481
slavam2605 <slavam2605@bk.ru>
2014-12-04 20:49:52 +0300

    small fix

--- app/src/main/java/com/example/home/superwheather/Alarm.java
+++ app/src/main/java/com/example/home/superwheather/Alarm.java
@@ -40,7 +40,7 @@ public class Alarm extends BroadcastReceiver
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, Alarm.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 60, pi);
+        am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 60, pi);
     }
 
     public void CancelAlarm(Context context)
