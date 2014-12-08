0b1548f90b2699f019099bd454f59084c98e0ae1
Dmitry Tomp <dmitrytomp@gmail.com>
2014-12-08 23:29:57 +0000

    AlarmManager exists

--- app/src/main/java/ru/ifmo/md/lesson8/WeatherUpdater.java
+++ app/src/main/java/ru/ifmo/md/lesson8/WeatherUpdater.java
@@ -1,6 +1,8 @@
 package ru.ifmo.md.lesson8;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Intent;
 
@@ -18,6 +20,10 @@ public class WeatherUpdater extends IntentService {
             } else {
                 resolver.update(intent.getData(), null, null, null);
                 resolver.notifyChange(intent.getData(), null, false);
+                AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                manager.set(AlarmManager.RTC_WAKEUP,
+                        System.currentTimeMillis() + AlarmManager.INTERVAL_HALF_DAY,
+                        PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT));
             }
     }
 }
