9e2a8d6b26a4ca2e99ee639a7178d86a9eed2863
izban <izban@mail.ru>
2015-01-10 19:46:25 +0300

    small bugs

--- app/src/main/java/ru/ifmo/md/lesson8/AddCityActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson8/AddCityActivity.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 
 public class AddCityActivity extends ActionBarActivity {
@@ -53,7 +54,6 @@ public class AddCityActivity extends ActionBarActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                text.setText(adapter.getItem(i));
                 startService(new Intent(AddCityActivity.this, CityLoadService.class).putExtra("city", adapter.getItem(i)));
                 finish();
             }
@@ -67,12 +67,43 @@ public class AddCityActivity extends ActionBarActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
                 Log.i("", "text changed!");
-                String s = charSequence.toString().toLowerCase();
-                int l = Collections.binarySearch(cities, s);
+                String s = charSequence.toString();
+                Comparator<String> comparator = new Comparator<String>() {
+                    // I don't know, why, but Integer.compare requires API level 19. I don't like that
+                    int compare(int a, int b) {
+                        if (a < b) {
+                            return -1;
+                        }
+                        if (a > b) {
+                            return 1;
+                        }
+                        return 0;
+                    }
+
+                    int compare(char a, char b) {
+                        if (a < b) {
+                            return -1;
+                        }
+                        if (a > b) {
+                            return 1;
+                        }
+                        return 0;
+                    }
+                    @Override
+                    public int compare(String s, String s2) {
+                        for (int i = 0; i < Math.min(s.length(), s2.length()); i++) {
+                            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s2.charAt(i))) {
+                                return compare(Character.toLowerCase(s.charAt(i)), Character.toLowerCase(s2.charAt(i)));
+                            }
+                        }
+                        return compare(s.length(), s2.length());
+                    }
+                };
+                int l = Collections.binarySearch(cities, s, comparator);
                 if (l < 0) {
                     l = (l + 1) * -1;
                 }
-                int r = Collections.binarySearch(cities, s + (char)(125));
+                int r = Collections.binarySearch(cities, s + "zzzzz", comparator); // Character.MAX_VALUE doesn't work :(
                 if (r < 0) {
                     r = (r + 1) * -1;
                 }
@@ -98,7 +129,7 @@ public class AddCityActivity extends ActionBarActivity {
                 if (s == null) {
                     break;
                 }
-                cities.add(s.toLowerCase());
+                cities.add(s);
             }
             Log.i("", cities.size() + " cities found");
         } catch (IOException e) {
