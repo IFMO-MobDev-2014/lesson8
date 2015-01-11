e830f087b359c29f0068d3cad691b85c7c67a1a3
izban <izban@mail.ru>
2015-01-11 09:27:27 +0300

    images

--- app/src/main/java/ru/ifmo/md/lesson8/ItemAdapter.java
+++ app/src/main/java/ru/ifmo/md/lesson8/ItemAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -19,6 +20,29 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         super(context, resource);
     }
 
+    private int getImage(String s) {
+        s = s.toLowerCase();
+        if (s.contains("rain") && s.contains("snow")) {
+            return R.drawable.snowrain;
+        }
+        if (s.contains("thunder")) {
+            return R.drawable.thunder;
+        }
+        if (s.contains("snow")) {
+            return R.drawable.snow;
+        }
+        if (s.contains("rain") || s.contains("shower")) {
+            return R.drawable.rain;
+        }
+        if (s.contains("wind")) {
+            return R.drawable.wind;
+        }
+        if (s.contains("fog") || s.contains("cloud")) {
+            return R.drawable.cloud;
+        }
+        return R.drawable.sun;
+    }
+
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         //View view = super.getView(position, convertView, parent);
@@ -30,11 +54,12 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         TextView date = (TextView)view.findViewById(R.id.textView);
         TextView temp = (TextView)view.findViewById(R.id.textView2);
         TextView cond = (TextView)view.findViewById(R.id.textView3);
+        ImageView image = (ImageView)view.findViewById(R.id.imageView);
 
         date.setText(new SimpleDateFormat("EEE, dd MMM yyyy").format(new Date(item.date)));
         temp.setText(item.low + " C ~ " + item.high + " C");
         cond.setText(item.text);
-
+        image.setImageResource(getImage(item.text));
         return view;
     }
 }
