a8fb4b35a964d3d7741fac6832dc6a6ce4af7b4e
denshvedov <denis.shvedov.95@mail.rudenis.shvedov.95@mail.rudenis.shvedov.95@mail.ru>
2015-02-04 23:38:13 +0300

    Done

---
+++ app/src/main/java/ru/ifmo/md/lesson8/WeatherLoaderClasses/WeatherLoader.java
@@ -0,0 +1,51 @@
+package ru.ifmo.md.lesson8.WeatherLoaderClasses;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+public class WeatherLoader extends IntentService {
+    public WeatherLoader() {
+        super("Weather loader");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        int woeid = intent.getIntExtra("woeid", 0);
+
+        String request = "https://query.yahooapis.com/v1/public/yql?q=";
+        String select = "select * from weather.forecast where woeid = " + String.valueOf(woeid);
+        try {
+            request += URLEncoder.encode(select, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        request += "&format=xml";
+
+        System.out.println(request);
+
+        URL url;
+        try {
+            url = new URL(request);
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            int responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                InputStream in = httpConnection.getInputStream();
+                XMLParser parser = new XMLParser(getApplicationContext(), woeid);
+                parser.parse(in);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
